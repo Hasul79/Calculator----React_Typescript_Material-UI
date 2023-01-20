c1d1c80e9d08d970d91b992685015d0c2dcfa182
Hasul79 <hatuk2@mail.ru>
2023-01-20 23:12:57 +0400

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Container , Grid , Paper , styled} from '@mui/material';
 
-
+const OutputContainer = styled('div')(({theme}) =>({
+  width: "100",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize:"3em",
+  overflow: "hidden"
+}))
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius:15
+}))
 function App() {
   return (
-    <div className="App">
-      <h1> Hasmik</h1>
-    </div>
+    <Container maxWidth="sm">
+      <CalculatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+             <OutputContainer>0</OutputContainer>
+          </Grid>
+        </Grid>
+      </CalculatorBase>
+    </Container>
   );
 }
 
